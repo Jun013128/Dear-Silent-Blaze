5be899c8db0053609dd2301e5f5e7439a68730ec
Jun013128 <jun0131.28@icloud.com>
2026-07-02 16:13:57 +0900

    HTML:headerとメニューページの修正。CSS：ハンバーガーメニュー作成中。JS：script.jsを追加。

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const
+menu = document.querySelector("#header__nav"),
+buttonOpen = document.querySelector("#header__btn--open"),
+buttonClose = document.querySelector("#header__btn--close");
+
+
+
+buttonOpen.addEventListener("click", () => {
+  // モーダルとしてひらく
+  menu.showModal();
+});
+buttonClose.addEventListener("click", () => {
+  // メニューをとじる
+  menu.close();
+});
+
+menu.addEventListener("click", (event) => {
+  // dialog本体クリックだけbackdrop扱い
+  if (event.target === menu) {
+    menu.close();
+  }
+});
+
+// メニュー内リンクを押したら閉じる
+for (const link of menu.querySelectorAll("a[href]")) {
+  link.addEventListener("click", () => {
+    menu.close();
+  });
+}
