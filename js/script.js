9d2860eac613746cf9fb3f0354ed1d359fd0a98d
Jun013128 <jun0131.28@icloud.com>
2026-07-05 02:25:18 +0900

    headerの修正、FVの作成

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 const
 menu = document.querySelector("#header__nav"),
 buttonOpen = document.querySelector("#header__btn-open"),
-buttonClose = document.querySelector("#header__btn-close");
+buttonClose = document.querySelector("#header__btn-close"),
+fv = document.querySelector(".fv"),
+fvTop = document.querySelector(".fv__top"),
+fvBottom = document.querySelector(".fv__bottom");
 
 
+// ハンバーガーメニュー
 
 buttonOpen.addEventListener("click", () => {
   // モーダルとしてひらく
@@ -27,3 +31,28 @@ for (const link of menu.querySelectorAll("a[href]")) {
     menu.close();
   });
 }
+
+// ファーストビュー
+
+window.addEventListener("load", () => {
+  fv.classList.add("is-active");
+
+  /* is-active後にtransitionを削除 */
+  setTimeout(() => {
+    fvTop.style.transition = "none";
+    fvBottom.style.transition = "none";
+  }, 1200); /* 画像フェードイン完了後 */
+});
+
+window.addEventListener("scroll", () => {
+  if (!fv.classList.contains("is-active")) return;
+
+  const scrollY = window.scrollY;
+  const fvHeight = fv.offsetHeight;
+  const scale = 1 + (scrollY / fvHeight) * 0.1;
+
+  if (scrollY <= fvHeight) {
+    fvTop.style.transform = `scale(${scale})`;
+    fvBottom.style.transform = `scale(${scale})`;
+  }
+});
